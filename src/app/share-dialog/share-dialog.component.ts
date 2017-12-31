e273ff32764c84b0f20f7bdf99d2d2825ba37fe5
christian@voegtle.org <christian@voegtle.org>
2017-12-31 16:08:01 +0100

    Url ins Clipboard kopieren

--- src/app/share-dialog/share-dialog.component.ts
+++ src/app/share-dialog/share-dialog.component.ts
@@ -8,4 +8,9 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 })
 export class ShareDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  copyClicked(url_field: HTMLInputElement) {
+    url_field.select();
+    document.execCommand('copy');
+  }
 }

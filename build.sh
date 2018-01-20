3e61e11ec490bc04a35ca55200fc3aaede4d0bfe
christian@voegtle.org <christian@voegtle.org>
2018-01-20 10:27:58 +0100

    Favicon für den Browser und Icon für die Integration in den Android Desktop

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ng build --prod
-appcache-manifest dist/*.{ico,html,js,webapp,css} --network-star -p /~christian/wunschmanager/ -o dist/ww.appcache
+appcache-manifest dist/*.{ico,html,js,webapp,css,png} dist/assets/*.{ico,html,js,webapp,css,png} --network-star -p /~christian/wunschmanager/ -o dist/ww.appcache

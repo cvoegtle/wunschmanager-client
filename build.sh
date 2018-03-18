75912f5321d1b9dcd71d086f27c8e5fd0b9082b0
christian@voegtle.org <christian@voegtle.org>
2018-03-18 09:46:05 +0100

    Versuch den Appcache zu aktivieren

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ng build --prod
-appcache-manifest dist/*.{ico,html,js,webapp,css,png} dist/assets/*.{ico,html,js,webapp,css,png} --network-star -p / -o dist/wm.appcache
+appcache-manifest dist/*.{ico,html,js,webapp,css,png} dist/assets/*.{ico,html,js,webapp,css,png} --network-star -p https://wunschmanager.appspot.com/ -o dist/wm.appcache
